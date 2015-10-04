e4cdc84081deb5ae74583822cd2538c9714bee6a
Jeremy Scheff <jdscheff@gmail.com>
2015-10-04 10:12:09 -0400

    UMD wrapper

--- faces.js
+++ faces.js
@@ -1,4 +1,17 @@
-window.faces = (function () {
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.faces = factory();
+    }
+}(this, function () {
     "use strict";
 
     var eye = [], eyebrow = [], hair = [], head = [], mouth = [], nose = [];
@@ -390,4 +403,4 @@ window.faces = (function () {
         display: display,
         generate: generate
     };
-}());
+}));
