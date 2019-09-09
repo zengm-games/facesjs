1a879acd5ec4c7f07cd851d6d5db6fc33392b86a
Jeremy Scheff <jdscheff@gmail.com>
2019-09-08 23:16:45 -0400

    Clean up

--- src/generate.js
+++ src/generate.js
@@ -18,36 +18,24 @@ const colors = [
 ];
 
 const generate = () => {
-  const face = {};
+  const eyeAngle = Math.random() * 50 - 20;
 
-  face.fatness = Math.random();
-
-  face.head = {
-    id: getID("head"),
-    color: colors[Math.floor(Math.random() * colors.length)]
+  const face = {
+    fatness: Math.random(),
+    head: {
+      id: getID("head"),
+      color: colors[Math.floor(Math.random() * colors.length)]
+    },
+    eye: { id: getID("eye"), angle: eyeAngle },
+    eyebrow: { id: getID("eyebrow") },
+    hair: { id: getID("hair") },
+    mouth: { id: getID("mouth") },
+    nose: {
+      id: getID("nose"),
+      flip: Math.random() < 0.5,
+      size: Math.random()
+    }
   };
-
-  const angle = Math.random() * 50 - 20;
-  face.eye = { id: getID("eye"), angle };
-  face.eyebrow = { id: getID("eyebrow") };
-  face.hair = { id: getID("hair") };
-  face.mouth = { id: getID("mouth") };
-  face.nose = {
-    id: getID("nose"),
-    flip: Math.random() < 0.5,
-    size: Math.random()
-  };
-
-  /*const id2 = getID(eyebrow);
-    face.eyebrows[0] = {id: id2, lr: "l", cx: 135, cy: 250};
-    face.eyebrows[1] = {id: id2, lr: "r", cx: 265, cy: 250};
-
-    const flip = Math.random() > 0.5 ? true : false;
-    face.nose = {id: getID(nose), lr: "l", cx: 200, cy: 330, size: Math.random(), posY: undefined, flip: flip};
-
-    face.mouth = {id: getID(mouth), cx: 200, cy: 400};
-
-    face.hair = {id: getID(hair)};*/
 
   return face;
 };
