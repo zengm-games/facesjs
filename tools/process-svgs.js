cabe38c4f6a1f1db99f5a9a00f85a29ae69571d0
Jeremy Scheff <jdscheff@gmail.com>
2019-09-09 22:57:35 -0400

    Better SVG processing and minification, using SVGO

--- tools/process-svgs.js
+++ tools/process-svgs.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 const path = require("path");
-const splitLines = require("split-lines");
+const SVGO = require("svgo");
 
-const processSVGs = () => {
+const svgo = new SVGO();
+
+const processSVGs = async () => {
   const svgFolder = path.join(__dirname, "..", "svg");
 
   const folders = fs.readdirSync(svgFolder);
@@ -15,19 +17,16 @@ const processSVGs = () => {
     const subfolder = path.join(svgFolder, folder);
     const files = fs.readdirSync(subfolder);
     for (const file of files) {
-      const contents = fs.readFileSync(path.join(subfolder, file), "utf8");
-      const lines = splitLines(contents)
-        .filter(
-          line =>
-            line !== "" &&
-            !line.includes("<?xml") &&
-            !line.includes("<svg") &&
-            !line.includes("</svg>")
-        )
-        .map(line => line.trim());
-
       const key = path.basename(file, ".svg");
-      svgs[folder][key] = lines.join("");
+
+      const contents = fs.readFileSync(path.join(subfolder, file), "utf8");
+      const result = await svgo.optimize(contents);
+      console.log("result", result);
+
+      // Replace <svg> and </svg> tags
+      svgs[folder][key] = result.data
+        .replace(/.*<svg.*?>/, "")
+        .replace("</svg>", "");
     }
   }
 
