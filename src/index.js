2ec96189fbfcdf48c7259f288b13d02fd5e40905
Jeremy Scheff <jdscheff@gmail.com>
2019-09-17 22:55:24 -0400

    Whoops

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ export { default as generate } from "./generate";
 // Usually not needed, but just in case...
 export { default as convertFromV1 } from "./convertFromV1";
 export { default as svgs } from "./svgs";
-export { default as svgsIndex } from "./svgsIndex";
+export { default as svgsIndex } from "./svgs-index";
