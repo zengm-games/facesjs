54b4c8b5dfab1c0dbf2244a4453771bdf718f29a
Jeremy Scheff <jdscheff@gmail.com>
2019-09-17 22:26:04 -0400

    More exports, just in case

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 export { default as display } from "./display";
 export { default as generate } from "./generate";
+
+// Usually not needed, but just in case...
+export { default as convertFromV1 } from "./convertFromV1";
+export { default as svgs } from "./svgs";
+export { default as svgsIndex } from "./svgsIndex";
