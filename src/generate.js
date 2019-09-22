b7b04a64ec3872a89164c97788e8ed8075fe1f2d
Travis <TravisJB89@gmail.com>
2019-09-21 21:55:41 -0700

    adding lines

--- src/generate.js
+++ src/generate.js
@@ -4,37 +4,81 @@ const getID = type => {
   return svgsIndex[type][Math.floor(Math.random() * svgsIndex[type].length)];
 };
 
-const skinColors = [
-  "#f2d6cb",
-  "#ddb7a0",
-  "#ce967d",
-  "#bb876f",
-  "#aa816f",
-  "#a67358",
-  "#ad6453",
-  "#74453d",
-  "#5c3937"
+const colors = [
+  {
+    skin: "#f2d6cb",
+    hair: ["#272421", "#423125", "#ab5513", "#e9c67b", "#D7BF91"]
+  },
+  { skin: "#ddb7a0", hair: ["#272421", "#423125", "#ab5513", "#e9c67b"] },
+  { skin: "#ce967d", hair: ["#272421", "#423125"] },
+  { skin: "#bb876f", hair: ["#272421"] },
+  { skin: "#aa816f", hair: ["#272421"] },
+  { skin: "#a67358", hair: ["#272421"] },
+  { skin: "#ad6453", hair: ["#272421"] },
+  { skin: "#74453d", hair: ["#272421"] },
+  { skin: "#5c3937", hair: ["#171411"] }
 ];
 
 const randomRounded = () => Math.round(Math.random() * 100) / 100;
 
 const generate = () => {
-  const eyeAngle = Math.round(Math.random() * 50 - 20);
+  const eyeAngle = randomRounded() * 25 - 10;
+
+  const palette = colors[Math.floor(Math.random() * colors.length)];
+  const skinColor = palette.skin;
+  const hairColor =
+    palette.hair[Math.floor(Math.random() * palette.hair.length)];
+  const jerseyColor = "#b44f4f";
+  const isFlipped = Math.random() < 0.5;
 
   const face = {
     fatness: randomRounded(),
+    body: {
+      id: getID("body"),
+      color: skinColor
+    },
+    jersey: {
+      id: getID("jersey"),
+      color: jerseyColor
+    },
+    ear: {
+      id: getID("ear"),
+      color: skinColor,
+      size: 0.75 + randomRounded() * 0.5
+    },
     head: {
       id: getID("head"),
-      color: skinColors[Math.floor(Math.random() * skinColors.length)]
+      color: skinColor
+      // shave: `rgba(0,0,0,${randomRounded() / 2})`
+    },
+    eyeline: {
+      id: getID("eyeline")
+    },
+    smileline: {
+      id: Math.random() > 0.75 ? "none" : getID("smileline"),
+      size: 0.5 + randomRounded()
+    },
+    miscline: {
+      id: Math.random() > 0.75 ? "none" : getID("miscline")
     },
     eye: { id: getID("eye"), angle: eyeAngle },
-    eyebrow: { id: getID("eyebrow") },
-    hair: { id: getID("hair") },
-    mouth: { id: getID("mouth") },
+    eyebrow: {
+      id: getID("eyebrow"),
+      angle: Math.round(Math.random() * 35 - 15),
+      thickness: 3 + Math.random() * 12
+    },
+    hair: {
+      id: getID("hair"),
+      color: hairColor
+    },
+    mouth: {
+      id: getID("mouth"),
+      flip: isFlipped
+    },
     nose: {
       id: getID("nose"),
-      flip: Math.random() < 0.5,
-      size: randomRounded()
+      flip: isFlipped,
+      size: 0.5 + randomRounded() * 0.75
     }
   };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 export { default as display } from "./display";
 export { default as generate } from "./generate";
-
-// Usually not needed, but just in case...
-export { default as convertFromV1 } from "./convertFromV1";
-export { default as svgs } from "./svgs";
-export { default as svgsIndex } from "./svgs-index";
