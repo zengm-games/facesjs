142245fcd9c6a68f0ddcf0b683e4ae489065f0b4
Jeremy Scheff <jdscheff@gmail.com>
2020-05-20 22:26:36 -0400

    Merge

--- src/generate.ts
+++ src/generate.ts
@@ -17,8 +17,8 @@ const colors = [
       "#2C1608",
       "#B55239",
       "#e9c67b",
-      "#D7BF91"
-    ]
+      "#D7BF91",
+    ],
   },
   {
     skin: "#ddb7a0",
@@ -28,10 +28,9 @@ const colors = [
       "#5A3825",
       "#CC9966",
       "#2C1608",
-      "#B55239",
       "#e9c67b",
-      "#D7BF91"
-    ]
+      "#D7BF91",
+    ],
   },
   { skin: "#ce967d", hair: ["#272421", "#423125"] },
   { skin: "#bb876f", hair: ["#272421"] },
@@ -39,7 +38,7 @@ const colors = [
   { skin: "#a67358", hair: ["#272421"] },
   { skin: "#ad6453", hair: ["#272421"] },
   { skin: "#74453d", hair: ["#272421"] },
-  { skin: "#5c3937", hair: ["#272421"] }
+  { skin: "#5c3937", hair: ["#272421"] },
 ];
 
 const defaultTeamColors = ["#0d435e", "#f0494a", "#cccccc"];
@@ -60,58 +59,59 @@ const generate = (overrides: Overrides) => {
     teamColors: defaultTeamColors,
     body: {
       id: getID("body"),
-      color: skinColor
+      color: skinColor,
     },
     jersey: {
-      id: getID("jersey")
+      id: getID("jersey"),
     },
     ear: {
       id: getID("ear"),
-      size: roundTwoDecimals(0.75 + Math.random() * 0.5)
+      size: roundTwoDecimals(0.75 + Math.random() * 0.5),
     },
     head: {
       id: getID("head"),
       shave: `rgba(0,0,0,${
         Math.random() < 0.25 ? roundTwoDecimals(Math.random() / 5) : 0
-      })`
+      })`,
     },
     eyeLine: {
-      id: Math.random() < 0.75 ? getID("eyeLine") : "none"
+      id: Math.random() < 0.75 ? getID("eyeLine") : "none",
     },
     smileLine: {
       id: Math.random() < 0.75 ? getID("smileLine") : "none",
-      size: roundTwoDecimals(0.5 + Math.random())
+      size: roundTwoDecimals(0.5 + Math.random()),
     },
     miscLine: {
-      id: Math.random() < 0.5 ? getID("miscLine") : "none"
+      id: Math.random() < 0.5 ? getID("miscLine") : "none",
     },
     facialHair: {
-      id: Math.random() < 0.5 ? getID("facialHair") : "none"
+      id: Math.random() < 0.5 ? getID("facialHair") : "none",
     },
     eye: { id: getID("eye"), angle: eyeAngle },
     eyebrow: {
       id: getID("eyebrow"),
-      angle: Math.round(Math.random() * 35 - 15)
+      angle: Math.round(Math.random() * 35 - 15),
     },
     hair: {
       id: getID("hair"),
-      color: hairColor
+      color: hairColor,
+      flip: isFlipped,
     },
     mouth: {
       id: getID("mouth"),
-      flip: isFlipped
+      flip: isFlipped,
     },
     nose: {
       id: getID("nose"),
       flip: isFlipped,
-      size: roundTwoDecimals(0.5 + Math.random() * 0.75)
+      size: roundTwoDecimals(0.5 + Math.random() * 0.75),
     },
     glasses: {
-      id: Math.random() < 0.1 ? getID("glasses") : "none"
+      id: Math.random() < 0.1 ? getID("glasses") : "none",
     },
     accessories: {
-      id: Math.random() < 0.2 ? getID("accessories") : "none"
-    }
+      id: Math.random() < 0.2 ? getID("accessories") : "none",
+    },
   };
 
   override(face, overrides);
