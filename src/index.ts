57b91b432e67db1f8f6ebb4ac55d037195384a45
Jeremy Scheff <jdscheff@gmail.com>
2023-02-21 14:01:00 -0500

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-export { default as display } from "./display";
+export { display } from "./display";
 
 // Switch back to one line like the others after TypeScript 3.8, see below
-import generate from "./generate";
+import { generate } from "./generate";
 export { generate };
 
 // Usually not needed, but just in case...
 export { default as svgs } from "./svgs";
-export { default as svgsIndex } from "./svgs-index";
+export { svgsIndex } from "./svgs-index";
 
 // Uncomment this when TypeScript 3.8 is out. Otherwise importing and re-exporting breaks
 // export type { Face } from "./generate";
