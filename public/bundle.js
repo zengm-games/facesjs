15a8023baee752a48f6497089ff14a3faf5ce9fc
Thomas Kennedy <tkennedy@smu.edu>
2024-04-01 19:32:34 -0700

    Some work

--- public/bundle.js
+++
@@ -1,5 +0,0 @@
-export { display, buildSVGString } from "../src/display";
-export { generate } from "../src/generate";
-export { svgsIndex } from "../src/svgs-index";
-
-export { default as override } from "../src/override";
