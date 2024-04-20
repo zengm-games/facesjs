f35aff6534d42369c5a56c5a01d1721e120b39aa
Thomas Kennedy <tkennedy@smu.edu>
2024-04-19 21:01:44 -0500

    Index page static

--- public/App.tsx
+++ public/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NextUIProvider } from "@nextui-org/react";
-import { Home } from "./Home";
 import { EditorPage } from "./EditorPage";
 
 const App = () => {
@@ -9,12 +8,22 @@ const App = () => {
     <NextUIProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<IndexStaticHTML />} />
           <Route path="/editor" element={<EditorPage />} />
           <Route path="/editor/:param" element={<EditorPage />} />
         </Routes>
       </BrowserRouter>
     </NextUIProvider>
+  );
+};
+
+const IndexStaticHTML = () => {
+  return (
+    <iframe
+      src="/index.html"
+      style={{ border: "none", width: "100%", height: "100vh" }}
+      title="FacesJS Home"
+    />
   );
 };
 
