52b1a13ef92782687886587b76183a1973f8fc30
Jeremy Scheff <jdscheff@gmail.com>
2024-04-21 07:09:28 -0400

    esbuild -> vite for root index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,15 +13,6 @@ export default {
   theme: {
     extend: {},
   },
-  purge: {
-    safelist: [
-      "grid-cols-1",
-      "grid-cols-2",
-      "grid-cols-3",
-      "grid-cols-4",
-      "grid-cols-5",
-    ],
-  },
   darkMode: "class",
   plugins: [
     typography,
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,33 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import chokidar from "chokidar";
-import path from "node:path";
+import { resolve } from "node:path";
 import { processSVGs } from "./tools/lib/process-svgs.js";
 
 export default defineConfig({
-  base: "/editor/",
   build: {
-    outDir: "../../build-site/editor",
+    chunkSizeWarningLimit: 1000,
+    emptyOutDir: true,
+    outDir: "../../build-site",
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, "public/index.html"),
+        editor: resolve(import.meta.dirname, "public/editor/index.html"),
+      },
+    },
   },
   plugins: [
     react(),
     {
-      name: "watch-for-svg-changes",
-      async configureServer() {
+      name: "process-svgs",
+      async config() {
+        // Always run this, for both build and dev
         await processSVGs();
-
+      },
+      async configureServer() {
+        // Only during dev
         chokidar
-          .watch([path.join(__dirname, "svgs")], {
+          .watch([resolve(import.meta.dirname, "svgs")], {
             ignoreInitial: true,
           })
           .on("all", (event, filePath) => {
@@ -27,9 +37,5 @@ export default defineConfig({
       },
     },
   ],
-  root: "public/editor",
-  server: {
-    host: true,
-    strictPort: true,
-  },
+  root: "public",
 });
