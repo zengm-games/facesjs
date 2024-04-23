ee35913438b8b74dc19a081f8a47bd112fa0ad34
Jeremy Scheff <jdscheff@gmail.com>
2024-04-23 15:55:00 -0400

    Don't render face to hidden div

--- public/index.ts
+++ public/index.ts
@@ -8,14 +8,37 @@ const newFace = (i: number) => {
   display(`c${i}`, face);
 };
 
+const wide = window.matchMedia("(min-width: 666px)");
+let renderedWideFaces = false;
+
+const NUM_FACES_SMALL = 5;
+const NUM_FACES_WIDE = 12;
+
 const newFaces = () => {
-  for (let i = 0; i < 12; i++) {
+  const numFaces = wide.matches ? NUM_FACES_WIDE : NUM_FACES_SMALL;
+
+  for (let i = 0; i < numFaces; i++) {
     newFace(i);
+  }
+
+  if (wide.matches) {
+    renderedWideFaces = true;
   }
 };
 
 // Generate faces for initial page load
 newFaces();
+
+// Watch for resizing from small to large, because otherwise we can't render the extra faces while they are not shown because it's glitchy rendering to a hidden div
+wide.addEventListener("change", () => {
+  if (wide.matches && !renderedWideFaces) {
+    for (let i = NUM_FACES_SMALL; i < NUM_FACES_WIDE; i++) {
+      newFace(i);
+    }
+
+    renderedWideFaces = true;
+  }
+});
 
 // Generate new faces when "r" is pressed
 document.addEventListener(
